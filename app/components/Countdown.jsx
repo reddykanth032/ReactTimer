ffb730131492725f8c8bed65d0bf05fa839c7e10
Kanth Reddy <kanth.reddyb032@gmail.com>
2020-02-17 09:10:52 +0530

    added Navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,10 @@
+var  React=require("react");
+
+var Countdown=()=>{
+  return(
+    <h1> in CountDown Page</h1>
+
+  );
+};
+
+module.exports=Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,9 @@
+var React=require('react');
+
+var Timer=()=>{
+  return(
+    <h1>in Timer Page</h1>
+  );
+};
+
+module.exports=Timer;

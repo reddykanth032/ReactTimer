71528f60f90e185c2ee53af263a3a0ad213aeb42
Kanth Reddy <kanth.reddyb032@gmail.com>
2020-02-18 10:02:05 +0530

    added testcases for countdown

---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,39 @@
+var React =require('react');
+var ReactDOM=require('react-dom');
+var expect=require('expect');
+var $=require('jQuery');
+var TestUtils=require('react-addons-test-utils');
+
+var Countdown=require('Countdown');
+
+describe('Countdown',()=>{
+  it('should exist',()=>{
+    expect(Countdown).toExist();
+  });
+  describe('handleSetCountdown',()=>{
+    it('it should set state to started and countdown starts',(done)=>{
+      var countdown=TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(10);
+
+      expect(countdown.state.count).toBe(10);
+      expect(countdown.state.countdownStatus).toBe('started');
+      setTimeout(()=>{
+        expect(countdown.state.count).toBe(9);
+        done();
+      },1000);
+    });
+
+    it('it should set count to zero',(done)=>{
+      var countdown=TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(1);
+
+      expect(countdown.state.count).toBe(1);
+      setTimeout(()=>{
+        expect(countdown.state.count).toBe(0);
+        done();
+      },3000);
+    });
+
+
+  });
+});

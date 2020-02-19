731e2c07c3d71ae7390ad478bf7850ddcd039ef8
Kanth Reddy <kanth.reddyb032@gmail.com>
2020-02-19 09:43:17 +0530

    added edge cases for countdown

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -32,6 +32,10 @@ var Countdown=React.createClass({
       this.setState({
         count:newCount>=0?newCount:0
       });
+
+      if(newCount===0){
+        this.setState({countdownStatus:'stopped'});
+      }
     },1000);
   },
   handleSetCountdown:function(seconds){
@@ -44,6 +48,10 @@ var Countdown=React.createClass({
     this.setState({
       countdownStatus:newStatus
     });
+  },
+  componentWillUnMount:function(){
+    clearInterval(this.timer)
+    this.timer=undefined;
   },
   render:function(){
     var {count,countdownStatus} =this.state;
